e20205e464ac647a0b3d8e789292c8cb45966bcc
heffcodex <dev@heffcodex.pro>
2023-04-14 18:53:23 +0400

    init

--- config.go
+++ config.go
@@ -1,11 +1,20 @@
 package redix
 
-type XConfig struct {
-	Namespace string `mapstructure:"namespace" json:"namespace"`
-}
-
 type Config struct {
 	XConfig
 	DSN  string `mapstructure:"dsn" json:"dsn"`
 	Cert string `mapstructure:"cert" json:"cert"`
+}
+
+type XConfig struct {
+	Namespace string `mapstructure:"namespace" json:"namespace"`
+}
+
+func (c *XConfig) AppendNamespace(ns string) {
+	if c.Namespace == "" {
+		c.Namespace = ns
+		return
+	}
+
+	c.Namespace = c.Namespace + keyDelimiter + ns
 }
